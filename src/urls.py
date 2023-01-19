681fc75b3cfad298967ad4a0cd6d9f067e838186
Lovely Boy <120689599+AppleKevin1106@users.noreply.github.com>
2023-01-19 09:41:50 -0500

    Update urls.py

--- src/urls.py
+++ src/urls.py
@@ -27,5 +27,5 @@ urlpatterns = [
 
 # to load static/media files in development environment
 if settings.DEBUG:
-    #urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    #urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
